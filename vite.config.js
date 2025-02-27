b144f478ac964fd601d63c49fd1ef45db39bb1dc
sadeg <csadeg@et.esiea.fr>
2025-02-27 13:41:11 +0100

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: "/Projet_ReactJs/",  // 🔥 Assure-toi que cette ligne est bien là
-  plugins: [react()],
-})
